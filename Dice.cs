b2655dcf108b95af85b680460cfdeca1817d98a3
Guilherme Bandeira <band.92@hotmail.com>
2021-07-23 00:40:05 -0300

    Update refactoring create Dice.cs and modified Program.cs

---
+++ Dice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClasseRandom
+{
+    class Dice
+    {
+        public int AmountOfSides { get; set; }
+        public int RolledNumber { get; set; }
+
+        public int RolledDice()
+        { 
+            Random rd = new Random();
+            RolledNumber = rd.Next(1, AmountOfSides +1);
+            return RolledNumber;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClasseRandom
 {
@@ -6,68 +7,40 @@ namespace ClasseRandom
     {
         static void Main(string[] args)
         {
-            int inputNumber, rolledNumber;
-            bool restartProcess = true;
-            while (restartProcess)
-            {
-                bool invalidInput = true;
-                Write("Write a number from 1 to 100 to define a range: ");
-                inputNumber = int.Parse(ReadLine());
-                
-                if (inputNumber > 0 && inputNumber < 101)
-                {
-                    AllowedNumber(inputNumber, out rolledNumber);
-                    restartProcess = false;
-                }
-                else
-                {
-                    WriteLine("Number out of Range, please insert a valid number between 1 to 100!");
-                    BlockedNumber(ref restartProcess, ref invalidInput);
-                }
-
-            }
+            Dice myDice = new Dice();
+            
+            Write("Enter the amount of sides of your dice between 2 and 100: ");
+            myDice.AmountOfSides = GetAmountOfValidSides();
+            WriteLine("");
+            int rolledDice = myDice.RolledDice();
+            RolledDiceOddorEven(rolledDice);
+            
             WriteLine("Thank you for participation");
         }
 
-        private static void AllowedNumber(int inputNumber, out int rolledNumber)
+        public static int GetAmountOfValidSides()
         {
-            Random rd = new Random();
-            rolledNumber = rd.Next(1, inputNumber);
-            if (rolledNumber % 2 == 0)
+            int AmountOfValidSides = int.Parse(ReadLine());
+
+            if (AmountOfValidSides > 1 && AmountOfValidSides < 101)
             {
-                WriteLine($"The Rolled number is {rolledNumber} and it is Odd!");
+                return AmountOfValidSides;
+            }
+            Console.Clear();
+            WriteLine($"The input amount is: {AmountOfValidSides}");
+            Write("Amount out of range!\nPlease insert between 2 and 100: ");
+            return GetAmountOfValidSides();
+        }
+
+        static void RolledDiceOddorEven(int rolledDice)
+        {
+            if (rolledDice % 2 == 0)
+            {
+                WriteLine($"The Rolled number is {rolledDice} and it is Odd!");
             }
             else
             {
-                WriteLine($"The Rolled number is {rolledNumber} and it is Even!");
-            }
-            
-        }
-        private static void BlockedNumber(ref bool restartProcess, ref bool invalidInput)
-        {
-            while (invalidInput)
-            {
-                Write("Do you want to try again? [y/n]: ");
-                {
-                    string TryAgain = ReadLine().ToString();
-                    if (TryAgain == "Y" || TryAgain == "y")
-                    {
-                        WriteLine("");
-                        invalidInput = false;
-                    }
-                    else if (TryAgain == "N" || TryAgain == "n")
-                    {
-                        WriteLine("");
-                        invalidInput = false;
-                        restartProcess = false;
-                        WriteLine("Finished process");
-                    }
-                    else
-                    {
-                        WriteLine("");
-                        WriteLine("Invalid handwriting, please insert a valid one!");
-                    }
-                }
+                WriteLine($"The Rolled number is {rolledDice} and it is Even!");
             }
         }
 
